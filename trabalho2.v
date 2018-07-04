a4ebf232207cda15ed89f80a913a3009a8b523d9
amanditagroth <33296292+amanditagroth@users.noreply.github.com>
2018-07-04 23:52:35 +0000

    Update trabalho2.v

--- trabalho2.v
+++ trabalho2.v
@@ -1,21 +1,23 @@
-module trabalho2(
+module trabalho(
 
-    input CLOCK_50; //CLOCK_50 sempre para a placa altera de 1
-    output reg [7:0] LEDG;
+    input CLOCK_50, //CLOCK_50 sempre para a placa altera de 1
+    output reg [3:0] LEDG
 );
 
 reg [25:0] contador; //conter mais de 25 fios para receber os 50000000
 
-wire cmp = contador == 50000000; //cmp receberá contador quanto conter até os 50 milhoes recebe por segundo
+wire cmp = contador >= 50000000; //cmp receberá contador quanto conter até os 50 milhoes recebe por segundo
 
 always @(posedge CLOCK_50) begin
-    contador <= contador + 1; //é necessário fazer o contador++
+    
+    if (cmp) begin
+        LEDG <= ~LEDG; //quando verdadeiro o pisca ligará
+		  contador <= 0; //é necessário fazer o contador++
 
-    if (cmp) begin;
-        LEDG <= LEDG; //quando verdadeiro o pisca ligará
 
-    end else begin;
-        LEDG <= ~LEDG; //quando falso o pisca mantém no mesmo estado
+    end else begin
+        LEDG <= LEDG; //quando falso o pisca mantém no mesmo estado
+		  contador <= contador + 1; //é necessário fazer o contador++
 
     end
 
