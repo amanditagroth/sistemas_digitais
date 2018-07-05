849706c0e66ea5983204429271039e3eb4676561
amanditagroth <33296292+amanditagroth@users.noreply.github.com>
2018-07-05 00:32:53 +0000

    Update trabalho2.v

--- trabalho2.v
+++ trabalho2.v
@@ -1,10 +1,13 @@
 module trabalho(
 
     input CLOCK_50, //CLOCK_50 sempre para a placa altera de 1
-    output reg [3:0] LEDG
+	 input [9:0] SW,
+    output reg [7:0] LEDG
 );
 
-reg [25:0] contador; //conter mais de 25 fios para receber os 50000000
+reg [25:0] contador; //conter mais de 26 fios para receber os 50000000
+
+reg [1:0] troca;//2 bit, pq tem 1 e 0
 
 wire cmp = contador >= 50000000; //cmp receberá contador quanto conter até os 50 milhoes recebe por segundo
 
@@ -17,16 +20,22 @@ always @(posedge CLOCK_50) begin
 
     end else begin
         LEDG <= LEDG; //quando falso o pisca mantém no mesmo estado
-		  contador <= contador + 1; //é necessário fazer o contador++
+		  contador <= contador + SW; //é necessário fazer o contador++
 
     end
+	 
+	 
+	 
+	 if (SW[0]) begin
+		troca = 1;
+		
+	 end else begin
+	  troca = 2;
 
+	  end
 end
 
 endmodule
 
-
-
 //implementação:
-//falta inverter por segundos
-//fazer piscar mais devagar
+// falta trocar de limite do contador que ser´a o reg, e fazer alterar de ledg e sw 
