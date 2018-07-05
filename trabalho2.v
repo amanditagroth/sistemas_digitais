15c581285a6128cc595d45d24de08c18436bf7b9
amanditagroth <33296292+amanditagroth@users.noreply.github.com>
2018-07-05 01:07:39 +0000

    Update trabalho2.v

--- trabalho2.v
+++ trabalho2.v
@@ -1,38 +1,24 @@
 module trabalho(
 
-    input CLOCK_50, //CLOCK_50 sempre para a placa altera de 1
-	 input [9:0] SW,
-    output reg [7:0] LEDG
+    input [9:0] SW,
+    output reg [7:0] HEX0
 );
 
-reg [25:0] contador; //conter mais de 26 fios para receber os 50000000
-
-reg [1:0] troca;//2 bit, pq tem 1 e 0
-
-wire cmp = contador >= 50000000; //cmp receberá contador quanto conter até os 50 milhoes recebe por segundo
-
-always @(posedge CLOCK_50) begin
+always @(SW) begin
     
-    if (cmp) begin
-        LEDG <= ~LEDG; //quando verdadeiro o pisca ligará
-		  contador <= 0; //é necessário fazer o contador++
-
-
-    end else begin
-        LEDG <= LEDG; //quando falso o pisca mantém no mesmo estado
-		  contador <= contador + SW; //é necessário fazer o contador++
-
-    end
-	 
-	 
-	 
-	 if (SW[0]) begin
-		troca = 1;
-		
-	 end else begin
-	  troca = 2;
-
-	  end
+	 if (SW == 6'b100000) begin // A
+		HEX0 <= 8'b00001000;
+	 end
+	 else if (SW == 6'b101000) begin // B
+		HEX0 <= 8'b00000011;
+	 end
+	 else if (SW == 6'b110000) begin // C
+		HEX0 <= 8'b01000110;
+	 end
+	 else begin
+		HEX0 <= 8'b11111111;
+	 end
+    
 end
 
 endmodule
