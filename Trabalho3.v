80b51090e4907453882d51a7ec5c5ba6a4191279
amanditagroth <33296292+amanditagroth@users.noreply.github.com>
2018-07-05 22:06:04 -0300

    Update Trabalho3.v

--- Trabalho3.v
+++ Trabalho3.v
@@ -1,34 +1,29 @@
 module trabalho3(
-	input k = 1,
-   input l = 0,
-	input CLOCK_50;
-	output reset = 0,
-	output s = x;
+	input [10:0] k,
+	input [10:0] l,
+	input CLOCK_50 = 0;
+	output reset = 1,
+	output S = x;
 );
-	always @ (poseger CLOCK_50);
-	 if (reset);
-		
-	
-	for x (k, l + 20);
-		
-		if () begin;
-			k = k + 1
-		end	
-			
-		else begin;
-
+	always @ (poseger CLOCK_50) begin;
+		if (reset == 1);begin;
+			S <= S + (k * l);
 		end
+end	
+	
+endmodule
 
 
-			for n ;
-			
-				if() begin;
-					l = l + 1
-					
-				end
-				
-				else begin;
-				
-				end
-end
+
+
+module testbench;
+
+	reg[10:0] k;
+	reg[10:0] l;
+	reg reset = 1;
+	reg CLOCK_50 = 0;
+	wire [20:0] S;
+	
+	prody x (reset, CLOCK_50, k, l, S); //pq prody????
+	
 endmodule
