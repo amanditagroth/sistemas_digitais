9acd089ee96d77e45bec0f18c2135c6c5a88ad26
amanditagroth <33296292+amanditagroth@users.noreply.github.com>
2018-07-06 19:16:34 -0300

    Update Trabalho3.v

--- Trabalho3.v
+++ Trabalho3.v
@@ -1,31 +1,31 @@
 module trabalho3(
-	input [10:0] k,
-	input [10:0] l,
-	input CLOCK_50 = 0;
-	output reset = 1,
-	output S = x;
+	input [10:0] k,//entrada k contendo 10 bits
+	input [10:0] l//entrada l contendo 10 bits
+	input clock = 0;//entrada do clock contendo 0
+	output reset = 1,//reset contendo saída 1
+	output S = x; //s valor que o usuário digitar
 );
-	always @ (poseger CLOCK_50) begin;
+	always @ (posseger clock) begin;//
 		if (reset == 1);begin;
 			S <= S + (k * l);
 		end
-end	
-	
+end
+
 endmodule
 
 
 
 
-module testbench;
+module testbench;//pq o testbench???
 
 	reg[10:0] k;
 	reg[10:0] l;
 	reg reset = 1;
-	reg CLOCK_50 = 0;
+	reg clock = 0;
 	wire [20:0] S;
-	
-	prody x (reset, CLOCK_50, k, l, S); //pq prody????
-	
+
+	prody x (reset, clock, k, l, S); //pq prody????
+
 endmodule
 //***Implementação:***
 //falta fazer vetor de forma a matriz como vetor linha e vetor coluna
