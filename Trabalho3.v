1062a9f12fabb7d315719f8677e5c0ba078776af
amanditagroth <33296292+amanditagroth@users.noreply.github.com>
2018-07-06 00:19:44 +0000

    Create Trabalho3.v

---
+++ Trabalho3.v
@@ -0,0 +1,34 @@
+module trabalho3(
+	input k = 1,
+   input l = 0,
+	input CLOCK_50;
+	output reset = 0,
+	output s = x;
+);
+	always @ (poseger CLOCK_50);
+	 if (reset);
+		
+	
+	for x (k, l + 20);
+		
+		if () begin;
+			k = k + 1
+		end	
+			
+		else begin;
+
+		end
+
+
+			for n ;
+			
+				if() begin;
+					l = l + 1
+					
+				end
+				
+				else begin;
+				
+				end
+end
+endmodule
