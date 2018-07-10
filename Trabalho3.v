c30769bf3b9840d3edfad7225b7b025184322bb9
amanditagroth <33296292+amanditagroth@users.noreply.github.com>
2018-07-09 21:47:55 -0300

    Update Trabalho3.v

--- Trabalho3.v
+++ Trabalho3.v
@@ -1,12 +1,19 @@
 module trabalho3(
 	input [10:0] k,//entrada k contendo 10 bits
-	input [10:0] l//entrada l contendo 10 bits
-	input clock = 0;//entrada do clock contendo 0
-	output reset = 1,//reset contendo saída 1
-	output S = x; //s valor que o usuário digitar
+	input [10:0] l,//entrada l contendo 10 bits
+	input clock,//entrada do clock contendo 0
+	input reset,//reset contendo saída 1
+	output [21:0] O
 );
-	always @ (posseger clock) begin;//
-		if (reset == 1);begin;
+
+assign O = S;
+
+reg [21:0] S;
+
+	always @ (posedge clock) begin//
+		if (reset == 1)begin
+		  S<=0;
+		end else begin
 			S <= S + (k * l);
 		end
 end
@@ -20,12 +27,29 @@ module testbench;//pq o testbench???
 
 	reg[10:0] k;
 	reg[10:0] l;
-	reg reset = 1;
+	wire[10:0] l2 = l;
+	reg reset ;
 	reg clock = 0;
-	wire [20:0] S;
+	wire [21:0] S;
 
-	prody x (reset, clock, k, l, S); //pq prody????
+  always #2 clock <= ~clock;
+
+	trabalho3 x (k, l2, clock,reset, S);//arquivo dentro do x edentro do testbench para testar
+		initial begin
+		    $dumpvars;//entrar com GTKWave
+			reset = 1;
+				#7; //iniciando os vetores da matrizes
+				reset = 0;
+				k = 3;
+				l = 5;
+				#4;
+				k = 2;
+				l = 9;
+				#4;
+				k = 8;
+				l = 3;
+				#10;
+				$finish;//saída do GTKWave
+		end
 
 endmodule
-//***Implementação:***
-//falta fazer vetor de forma a matriz como vetor linha e vetor coluna
